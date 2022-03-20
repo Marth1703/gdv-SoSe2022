f4e28d28d55a09f8d0274416cffa259bfe843419
Marth1703 <marvin.fischer@hs-furtwangen.de>
2022-03-20 18:40:44 +0100

    Half of the first task

--- GDV_tutorial_01_empty.py
+++ GDV_tutorial_01_empty.py
@@ -1,7 +1,7 @@
 # TODO first step is to import the opencv module which is called 'cv2'
-
+import cv2
 # TODO check the opencv version
-
+print(cv2.__version__)
 # TODO load an image with image reading modes using 'imread'
 # cv2.IMREAD_UNCHANGED  - If set, return the loaded image as is (with alpha
 #                         channel, otherwise it gets cropped). Ignore EXIF
@@ -10,7 +10,7 @@
 #                         grayscale image (codec internal conversion).
 # cv2.IMREAD_COLOR      - If set, always convert image to the 3 channel BGR
 #                         color image.
-
+img = cv2.imread("images/chessboard-contrast-squares.jpg", cv2.IMREAD_GRAYSCALE)
 
 # TODO resize image with 'resize'
 
